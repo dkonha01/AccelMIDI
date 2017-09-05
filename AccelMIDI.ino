ee74846dac59959e45ca791182feb19c30ea2b4e
dkonha01 <dkonhauser@gmail.com>
2017-09-04 18:02:56 -0700

    diff4

--- AccelMIDI.ino
+++ AccelMIDI.ino
@@ -26,10 +26,9 @@ void loop() {
   //  int sumXY = ((readX + readY)/13);
 
   int diff2x = abs(readX - readY);
-  //  int diff2x = abs(readX - readY);
-
-   int diff3x = abs((readX*3) - readY);
-  //  int diff2x = abs(readX - readY);
+  int diffx3 = abs((readX*3) - readY);
+  int diff4 = abs(readX + readY)/4;
+ 
 
   AccelerationReading currentAccel = Bean.getAcceleration();    
   int accelDifference = getAccelDifference(previousAccel, currentAccel);   
@@ -40,13 +39,15 @@ void loop() {
 
         BeanMidi.noteOn(CHANNEL1, sumXY, 11);  
         BeanMidi.noteOn(CHANNEL1, readY, sumXY);  
+        BeanMidi.noteOn(CHANNEL1, diffx3, diff2x);  
        
     }   
 
     else if (accelDifference > THRESHOLD_1){
 
         BeanMidi.noteOn(CHANNEL1, readX, 9);  
-        BeanMidi.noteOn(CHANNEL1, diff2x, sumXY);                     
+        BeanMidi.noteOn(CHANNEL1, diff2x, sumXY);
+                             
            
     }
 
